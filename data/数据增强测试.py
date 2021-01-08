31e2bdb90c0b39e65e10b0c0e3a7ec83ce834ae5
ziv <zeranxu@foxmail.com>
2021-01-08 16:32:47 +0800

    albumentations数据增强

--- data/数据增强测试.py
+++ data/数据增强测试.py
@@ -19,6 +19,9 @@ transform = A.Compose([
     A.RandomRain(always_apply=True),
     A.GaussNoise(always_apply=True),
     A.MultiplicativeNoise(always_apply=True),
+    # A.RandomFog(always_apply=True),
+    A.GaussianBlur(always_apply=True),
+    # A.ColorJitter(always_apply=True),
     # A.OpticalDistortion(always_apply=True,distort_limit=2, shift_limit=0.5)
 ])
 
@@ -34,14 +37,16 @@ for i, tiff in enumerate(tqdm.tqdm(tiffs)):
     # bbox[:,:4]=data[:,1:]
     # bbox[:,4]=data[:,0]
 
+    nums = int(np.random.random() * 300)
+    lines = (np.random.random(nums) * tiff1.shape[1]).astype(np.int)
+    tiff1[:, lines, 1] = 30
+
     transformed = transform(image=tiff1)
     transformed_image = transformed['image']
     # transformed_bboxes = transformed['bboxes']
     # transformed_class_labels = transformed['class_labels']
 
-    nums = int(np.random.random() * 300)
-    lines = (np.random.random(nums) * tiff1.shape[1]).astype(np.int)
-    transformed_image[:, lines, 1] = 30
+
 
     transformed_image[:, :, 2] = 0
     cv2.imencode('.png', transformed_image)[1].tofile(os.path.join(folder_new, os.path.split(tiff)[1]))
